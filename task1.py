35142898780f32119c260bc19a839d1e69466941
aldrinkileo <aldrinkileo@gmail.com>
2025-11-08 19:00:19 +0530

    Added the task1 python file of assignment 1

---
+++ task1.py
@@ -0,0 +1,12 @@
+num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+
+sum = num1 + num2
+remainder = num1 - num2
+product = num1 * num2
+division = num1 / num2
+
+print('Addition:',sum)
+print('Subtraction:',remainder)
+print('Multiplication:',product)
+print('division:',division)
